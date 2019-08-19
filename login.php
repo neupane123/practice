5d7dc95d966ba5d8223c217b2f62b5c49be80b2f
Aayan Neupane <aayanneupane25@gmail.com>
2019-08-19 15:16:50 +0545

    login page completed by aayan

--- login.php
+++ login.php
@@ -1,9 +1,40 @@
 <?php
 	$creds = require_once "credentials.php";
+	$err = [];
 
 	if(isset($_POST['submit']))
 	{
-		
+		if(!empty($_POST['email']))
+		{
+			$email = $_POST['email'];
+		}else{
+			$err['email']="email is required";
+		}
+
+		if(!empty($_POST['password']))
+		{
+			$password = $_POST['password'];
+		}else{
+			$err['password']="password is required";
+		}
+
+		if(count($err) == 0)
+		{
+			if(array_key_exists($email, $creds))
+			{	
+				if(password_verify($password, $creds[$email]['password']))
+				{
+					echo "<script>alert('login success')</script>";
+
+				}else{
+
+					$err['failed'] = "wrong password !";
+				}
+				
+			}else{
+				$err['failed'] = "account does not exists";
+			}
+		}
 	}
 
 ?>
